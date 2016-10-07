64f683fda5c191d58675113c1b90e4c2a8db7487
mono <monotom@gmail.com>
2016-10-07 14:29:27 +0200

    add create table command

--- lib/db/load_helper.rb
+++ lib/db/load_helper.rb
@@ -3,6 +3,8 @@ require 'capistrano/container'
 
 module LoadHelper
   def self.import_on_local()
+    LoadHelper.create_db_if_not_exists fetch(:db_name)
+
     run_locally do
       execute "mysql #{Helper::mysql_restore_args} < #{fetch(:db_local_dump)}"
     end
@@ -11,12 +13,20 @@ module LoadHelper
   def self.import_on_container(container)
     container.upload!(fetch(:db_local_dump), fetch(:db_remote_dump))
 
+    LoadHelper.create_db_if_not_exists fetch(:db_name)
+
     container.execute "mysql #{Helper::mysql_restore_args} < #{fetch(:db_remote_dump)}"
   end
 
   def self.import_on_server()
     upload!(fetch(:db_local_dump), fetch(:db_remote_dump))
 
+    LoadHelper.create_db_if_not_exists fetch(:db_name)
+
     execute("mysql #{Helper::mysql_restore_args} < #{fetch(:db_remote_dump)}")
+  end
+
+  def self.create_db_if_not_exists(db)
+    Helper.execute_db_command_autodetect "CREATE DATABASE IF NOT EXISTS #{db};"
   end
 end
